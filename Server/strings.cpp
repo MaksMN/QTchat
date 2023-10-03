693d6d20464aaf5789b1098719a7d2495395a4a8
MaksMN <maksimovmn78@gmail.com>
2023-10-03 23:13:46 +0300

    Класс хранения строк

---
+++ Server/strings.cpp
@@ -0,0 +1,8 @@
+#include "strings.h"
+
+const QString Strings::MY_STRING = QObject::tr("Hello, world!");
+const QString Strings::MY_STRING2 = QObject::tr("Hello, world!2");
+Strings::Strings()
+{
+
+}
---
+++ Server/strings.h
@@ -0,0 +1,15 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+#include <QObject>
+
+class Strings
+{
+
+public:
+    Strings();
+    static const QString MY_STRING;
+    static const QString MY_STRING2;
+};
+
+#endif // STRINGS_H
