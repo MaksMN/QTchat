df78124095ad9c4374d749378083338995ad432a
MaksMN <maksimovmn78@gmail.com>
2023-10-07 11:58:25 +0300

    project service files

---
+++ ChatStructs/ChatStructs_global.h
@@ -0,0 +1,12 @@
+#ifndef CHATSTRUCTS_GLOBAL_H
+#define CHATSTRUCTS_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#if defined(CHATSTRUCTS_LIBRARY)
+#  define CHATSTRUCTS_EXPORT Q_DECL_EXPORT
+#else
+#  define CHATSTRUCTS_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif // CHATSTRUCTS_GLOBAL_H
