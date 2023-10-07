a7b8fff5acdab5946288ff6f4f9eb057483c6f22
MaksMN <maksimovmn78@gmail.com>
2023-10-07 11:53:52 +0300

    Updating the project structure

---
+++ Server/DB/db.h
@@ -0,0 +1,12 @@
+#ifndef DB_H
+#define DB_H
+
+#include <QObject>
+
+class DB
+{
+public:
+    DB();
+};
+
+#endif // DB_H
--- Server/strings.cpp
+++
@@ -1,8 +0,0 @@
-#include "strings.h"
-
-const QString Strings::MY_STRING = QObject::tr("Hello, world!");
-const QString Strings::MY_STRING2 = QObject::tr("Hello, world!2");
-Strings::Strings()
-{
-
-}
