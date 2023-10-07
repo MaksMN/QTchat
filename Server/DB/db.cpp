b00620d91326e970603fa2f63dec1ef0ac203e37
MaksMN <maksimovmn78@gmail.com>
2023-10-08 00:05:21 +0300

    Development of database classes

--- Server/DB/db.cpp
+++ Server/DB/db.cpp
@@ -2,5 +2,16 @@
 
 DB::DB()
 {
-
+    CSimpleIniA ini; // https://github.com/brofield/simpleini/tree/master
+    SI_Error rc = ini.LoadFile("dserver.ini");
+    if (rc < 0) {
+        QMessageBox msgBox;
+        QIcon icon("app.ico");
+        msgBox.setWindowIcon(icon);
+        msgBox.setText(Strings::t(Strings::SERVER_INI_NOTFOUND));
+        msgBox.setInformativeText(Strings::t(Strings::DB_USE_DEFAULT_SETTINGS));
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    };
 }
--- Server/DB/db.h
+++ Server/DB/db.h
@@ -1,12 +1,31 @@
 #ifndef DB_H
 #define DB_H
 
+#include <QMessageBox>
 #include <QObject>
+#include "../strings/strings.h"
+#include "SimpleIni.h"
+
+typedef unsigned int uint;
+typedef unsigned long long ullong;
+const extern char config_file[];
 
 class DB
 {
+protected:
+    std::string server = "127.0.0.1";
+    std::string port = "5432";
+    std::string dbuser = "qt_chat";
+    std::string dbpass = "qt_chat";
+    std::string dbname = "qt_chat";
+    std::string odbc_driver = "PostgreSQL ANSI";
+    std::string db_character_set = "UTF8";
+    uint db_errno = 0;
+
 public:
     DB();
+
+    virtual ~DB() = default;
 };
 
 #endif // DB_H
