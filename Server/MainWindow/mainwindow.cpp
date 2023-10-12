43cf0d9a615aab2cb31cf6eec94dc8debafc2902
MaksMN <maksimovmn78@gmail.com>
2023-10-13 00:21:35 +0300

    User list element

--- Server/MainWindow/mainwindow.cpp
+++ Server/MainWindow/mainwindow.cpp
@@ -2,12 +2,24 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include "./ui_mainwindow.h"
+#include "user.h"
+#include "userwidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    UserWidget *userwidget = new UserWidget();
+    chat::User user(1, "login", "name", "family", chat::user::common);
+
+    userwidget->setUser(&user);
+
+    QListWidgetItem *item1 = new QListWidgetItem();
+    item1->setSizeHint(userwidget->sizeHint());
+    ui->listUsers->addItem(item1);
+    ui->listUsers->setItemWidget(item1, userwidget);
 }
 
 MainWindow::~MainWindow()
