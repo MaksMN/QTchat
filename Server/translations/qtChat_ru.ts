d89a130fe601fd1e37b03f2cf8b6fbe568037738
MaksMN <maksimovmn78@gmail.com>
2023-10-14 02:12:32 +0300

    other files

---
+++ Server/translations/qtChat_ru.ts
@@ -0,0 +1,179 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="14"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="225"/>
+        <source>qtChat - Server</source>
+        <translation>qtChat - Сервер</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="56"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="228"/>
+        <source>Users</source>
+        <translation>Пользователи</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="90"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="229"/>
+        <source>User actions</source>
+        <translation>Действия с пользователями</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="99"/>
+        <location filename="../MainWindow/mainwindow.ui" line="197"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="230"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="235"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="106"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="231"/>
+        <source>Ban</source>
+        <translation>Бан</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="113"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="232"/>
+        <source>Unlogin</source>
+        <translation>Отключить</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="154"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="233"/>
+        <source>Messages</source>
+        <translation>Сообщения</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="188"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="234"/>
+        <source>Messages actions</source>
+        <translation>Действия с соощениями</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="204"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="236"/>
+        <source>Hide</source>
+        <translation>Скрыть</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="231"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="237"/>
+        <source>Main</source>
+        <translation>Главное меню</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="244"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="226"/>
+        <source>Shut Down</source>
+        <translation>Остановить и закрыть</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow/mainwindow.ui" line="252"/>
+        <location filename="../build/debug/Server_autogen/include/ui_mainwindow.h" line="227"/>
+        <source>Server Settings</source>
+        <translation type="unfinished">Настройки сервера</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../Strings/strings.cpp" line="3"/>
+        <source>File server.ini not found</source>
+        <translation>Не найден файл server.ini</translation>
+    </message>
+    <message>
+        <location filename="../Strings/strings.cpp" line="4"/>
+        <source>Default settings will be used to connect to the database</source>
+        <translation>Для подключения к базе данных будут использованы настройки по умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../Strings/strings.cpp" line="6"/>
+        <source>Settings cannot be saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Strings/strings.cpp" line="8"/>
+        <source>Не удалось сохранить настройки в ini файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ServerSettings</name>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="14"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="229"/>
+        <source>Server Settings</source>
+        <translation>Настройки сервера</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="27"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="230"/>
+        <source>This Server</source>
+        <translation>Этот сервер</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="37"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="231"/>
+        <source>Address:</source>
+        <translation>Адрес:</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="44"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="232"/>
+        <source>Port:</source>
+        <translation>Порт:</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="88"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="233"/>
+        <source>Server DB</source>
+        <translation>Сервер БД</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="98"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="234"/>
+        <source>Address: </source>
+        <translation>Адрес: </translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="105"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="235"/>
+        <source>Port: </source>
+        <translation>Порт: </translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="112"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="236"/>
+        <source>DB user:</source>
+        <translation>Пользователь БД:</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="119"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="237"/>
+        <source>DB password:</source>
+        <translation>Пароль БД:</translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="126"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="238"/>
+        <source>DB name: </source>
+        <translation>Имя БД: </translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="133"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="239"/>
+        <source>ODBC Driver: </source>
+        <translation>Драйвер ODBC: </translation>
+    </message>
+    <message>
+        <location filename="../ServerSettings/serversettings.ui" line="140"/>
+        <location filename="../build/debug/Server_autogen/include/ui_serversettings.h" line="240"/>
+        <source>DB Charset: </source>
+        <translation>Кодировка БД: </translation>
+    </message>
+</context>
+</TS>
