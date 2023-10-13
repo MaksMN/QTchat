36efc43d72c03b019d6870640b7b496769797ae3
MaksMN <maksimovmn78@gmail.com>
2023-10-14 02:11:52 +0300

    new strings

--- Server/Strings/strings.cpp
+++ Server/Strings/strings.cpp
@@ -3,6 +3,10 @@
 const QString Strings::SERVER_INI_NOTFOUND = QObject::tr("File server.ini not found");
 const QString Strings::DB_USE_DEFAULT_SETTINGS = QObject::tr(
     "Default settings will be used to connect to the database");
+const QString Strings::SETTINGS_CANNOT_BE_SAVED = QObject::tr("Settings cannot be saved");
+
+const QString Strings::FAILED_TO_SAVE_SETTINGS_TO_INI_FILE = QObject::tr(
+    "Не удалось сохранить настройки в ini файл");
 
 QString Strings::t(const QString &s)
 {
--- Server/Strings/strings.h
+++ Server/Strings/strings.h
@@ -5,12 +5,17 @@
 #include <QString>
 #include <QTranslator>
 
+/*!
+ * \brief Класс для хранения строковых переменных. Для них работает перевод.
+ */
 class Strings : public QObject
 {
 private:
 public:
     static const QString SERVER_INI_NOTFOUND;
     static const QString DB_USE_DEFAULT_SETTINGS;
+    static const QString SETTINGS_CANNOT_BE_SAVED;
+    static const QString FAILED_TO_SAVE_SETTINGS_TO_INI_FILE;
     static QString t(const QString &s);
 };
 
