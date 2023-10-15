36138b1c1dd94bf6f41f1e824d162a39520cca85
MaksMN <maksimovmn78@gmail.com>
2023-10-15 12:09:44 +0300

    CircularQueue optimization

--- Misc/circularqueue.h
+++ Misc/circularqueue.h
@@ -4,15 +4,15 @@
 /*!
  * \brief Круговая очередь. При добавлении элемента удаляется элемент с другой стороны
  */
-template <typename T, typename N = int>
+template<typename T, typename N = int>
 class CircularQueue
 {
 private:
     std::deque<T> _deque;
-    N _size = 20;
+    N _max_size = 20;
 
 public:
-    explicit CircularQueue(N size = 20);
+    explicit CircularQueue(N max_size = 20);
     ~CircularQueue() = default;
     void push_front(T &obj);
     void push_front(T &&obj);
@@ -22,21 +22,28 @@ public:
     void pop_back();
     void resize_front(N new_size);
     void resize_back(N new_size);
+    /*!
+     * \brief Максимальное количество элементов очереди
+     */
+    N max_size();
+    /*!
+     * Количество элементов в очереди
+     */
     N size();
     T operator[](N index);
     std::deque<T> &decue();
 };
 
 template<typename T, typename N>
-inline CircularQueue<T, N>::CircularQueue(N size)
-    : _size(size)
+inline CircularQueue<T, N>::CircularQueue(N max_size)
+    : _max_size(size)
 {}
 
 template <typename T, typename N>
 inline void CircularQueue<T, N>::push_front(T &obj)
 {
     _deque.push_front(obj);
-    if (_deque.size() > _size)
+    if (_deque.size() > _max_size)
         _deque.pop_back();
 }
 template <typename T, typename N>
@@ -55,7 +62,7 @@ template <typename T, typename N>
 inline void CircularQueue<T, N>::push_back(T &obj)
 {
     _deque.push_back(obj);
-    if (_deque.size() > _size)
+    if (_deque.size() > _max_size)
         _deque.pop_front();
 }
 template <typename T, typename N>
@@ -77,7 +84,7 @@ inline void CircularQueue<T, N>::resize_front(N new_size)
             _deque.pop_front();
         }
     }
-    _size = new_size;
+    _max_size = new_size;
 }
 template<typename T, typename N>
 inline void CircularQueue<T, N>::resize_back(N new_size)
@@ -87,18 +94,23 @@ inline void CircularQueue<T, N>::resize_back(N new_size)
             _deque.pop_back();
         }
     }
-    _size = new_size;
+    _max_size = new_size;
+}
+template<typename T, typename N>
+inline N CircularQueue<T, N>::max_size()
+{
+    return _max_size;
 }
 template<typename T, typename N>
 inline N CircularQueue<T, N>::size()
 {
-    return _size;
+    return _deque.size();
 }
 
 template<typename T, typename N>
 inline T CircularQueue<T, N>::operator[](N index)
 {
-    if (index < 0 || index >= _size)
+    if (index < 0 || index >= _max_size)
         return T();
     return _deque[index];
 }
