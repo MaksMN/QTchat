c087c13da3a5d272c4d53101b48206e8beb415f5
MaksMN <maksimovmn78@gmail.com>
2023-10-15 12:14:53 +0300

    added new strings

--- Server/Strings/strings.cpp
+++ Server/Strings/strings.cpp
@@ -1,12 +1,20 @@
 #include "strings.h"
 
+/*Strings*/
+const QString Strings::DATABASE_ERROR = QObject::tr("Database error");
+const QString Strings::FAILED_CONNECTION_TO_THE_DATABASE = QObject::tr("Failed connection to the database");
+const QString Strings::CONNECTION_TO_THE_DATABASE_WAS_SUCCESSFUL = QObject::tr("Connection to the database was successful");
+const QString Strings::CONNECTING_TO_THE_DATABASE_SERVER = QObject::tr(
+    "Connecting to the database server...");
+const QString Strings::SETTINGS_SAVED_RESTART_THE_SERVER = QObject::tr(
+    "Settings are saved, restart the server.");
 const QString Strings::SERVER_INI_NOTFOUND = QObject::tr("File server.ini not found");
 const QString Strings::DB_USE_DEFAULT_SETTINGS = QObject::tr(
     "Default settings will be used to connect to the database");
 const QString Strings::SETTINGS_CANNOT_BE_SAVED = QObject::tr("Settings cannot be saved");
-
 const QString Strings::FAILED_TO_SAVE_SETTINGS_TO_INI_FILE = QObject::tr(
-    "Не удалось сохранить настройки в ini файл");
+    "Failed to save settings to ini file");
+/********/
 
 QString Strings::t(const QString &s)
 {
--- Server/Strings/strings.h
+++ Server/Strings/strings.h
@@ -12,10 +12,18 @@ class Strings : public QObject
 {
 private:
 public:
+    /*Strings*/
+	static const QString DATABASE_ERROR;
+	static const QString FAILED_CONNECTION_TO_THE_DATABASE;
+	static const QString CONNECTION_TO_THE_DATABASE_WAS_SUCCESSFUL;	
+    static const QString THE_TEST_TEXT;
+    static const QString CONNECTING_TO_THE_DATABASE_SERVER;
+    static const QString SETTINGS_SAVED_RESTART_THE_SERVER;
     static const QString SERVER_INI_NOTFOUND;
     static const QString DB_USE_DEFAULT_SETTINGS;
     static const QString SETTINGS_CANNOT_BE_SAVED;
     static const QString FAILED_TO_SAVE_SETTINGS_TO_INI_FILE;
+    /********/
     static QString t(const QString &s);
 };
 
