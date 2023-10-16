748ea627e19b35986f05c2209d3f6d738d33851a
MaksMN <maksimovmn78@gmail.com>
2023-10-16 22:54:43 +0300

    console output below

--- Server/Console/console.cpp
+++ Server/Console/console.cpp
@@ -1,23 +1,41 @@
 #include "console.h"
-QTextBrowser *Console::_console = nullptr;
-CircularQueue<QString, int> Console::_lines(255);
+#include <QApplication>
+#include <QScrollBar>
+#include <QtDebug>
 
-Console::Console() {}
+Console::Console(QTextBrowser *console)
+    : _console(console)
+{}
 
 void Console::setConsole(QTextBrowser *console)
 {
     _console = console;
 }
 
-void Console::writeLine(QString line)
+QTextBrowser *Console::getConsole()
+{
+    return _console;
+}
+
+void Console::writeLine(QString &line)
 {
     if (_console == nullptr) {
-        QDebug("Console not found");
+        qDebug() << line;
+        return;
     }
-    _lines.push_front(line);
+    _lines.push_back(line);
     QString lines;
-    for (int i = 0; i < _lines.decue().size(); ++i) {
+    for (int i = 0; i < _lines.size(); ++i) {
         lines += _lines[i] + '\n';
     }
     _console->setText(lines);
+    QScrollBar *scrollBar = _console->verticalScrollBar();
+    scrollBar->setValue(scrollBar->maximum());
+    _console->moveCursor(QTextCursor::End);
+    // QApplication::processEvents();
+}
+
+void Console::writeLine(QString &&line)
+{
+    writeLine(line);
 }
--- Server/Console/console.h
+++ Server/Console/console.h
@@ -1,21 +1,21 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-#include <QObject>
 #include <QTextBrowser>
-#include <QWidget>
 #include "circularqueue.h"
 
 class Console
 {
 private:
-    static QTextBrowser *_console;
-    static CircularQueue<QString, int> _lines;
+    QTextBrowser *_console;
+    CircularQueue<QString, int> _lines;
 
 public:
-    Console();
-    static void setConsole(QTextBrowser *console);
-    static void writeLine(QString line);
+    explicit Console(QTextBrowser *console);
+    void setConsole(QTextBrowser *console);
+    QTextBrowser *getConsole();
+    void writeLine(QString &line);
+    void writeLine(QString &&line);
 };
 
 #endif // CONSOLE_H
