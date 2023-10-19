1a097e1e8cb37ca2413d1793f41c83544071c544
MaksMN <maksimovmn78@gmail.com>
2023-10-19 19:20:19 +0300

    create item widget

--- ChatStructs/useritem.cpp
+++ ChatStructs/useritem.cpp
@@ -1,7 +1,29 @@
 #include "useritem.h"
 
-UserItem::UserItem(QWidget *parent)
-    : QWidget{parent}
+UserItem::UserItem()
 {
+    userWidget->setUser(nullptr);
+}
 
+UserItem::UserItem(const std::shared_ptr<chat::User> &user)
+
+    : _user(user)
+{
+    userWidget->setUser(_user);
+}
+
+void UserItem::Update()
+{
+    if (_user == nullptr) {
+        this->setHidden(true);
+        return;
+    }
+    userWidget->setUser(_user);
+    this->setHidden(false);
+}
+
+void UserItem::Update(const std::shared_ptr<chat::User> user)
+{
+    _user = user;
+    Update();
 }
--- ChatStructs/useritem.h
+++ ChatStructs/useritem.h
@@ -7,15 +7,22 @@
 #include "user.h"
 #include "userwidget.h"
 
-class UserItem : public QWidget, QListWidgetItem
+class UserItem : public QListWidgetItem
 {
     Q_OBJECT
 private:
+    std::shared_ptr<chat::User> _user = nullptr;
+    std::shared_ptr<UserWidget> userWidget{};
+
 public:
-    explicit UserItem(QWidget *parent = nullptr);
+    explicit UserItem();
+    UserItem(const std::shared_ptr<chat::User> &user = nullptr);
+
+    void Update();
+    void Update(const std::shared_ptr<chat::User> user);
+    void setUser(const std::shared_ptr<chat::User> user);
 
 signals:
-
 };
 
 #endif // USERITEM_H
