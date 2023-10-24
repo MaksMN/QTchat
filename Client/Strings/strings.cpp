ea2e45dd3f6c1244eead3a23b2d1816888a7ec55
MaksMN <maksimovmn78@gmail.com>
2023-10-24 15:57:49 +0300

    Разработка клиента

---
+++ Client/Strings/strings.cpp
@@ -0,0 +1,39 @@
+#include "strings.h"
+
+/*Strings*/
+const QString Strings::EMAIL_BUSY = QObject::tr("Email busy");
+const QString Strings::LOGIN_BUSY = QObject::tr("Login busy");
+const QString Strings::UNKNOWN_REASON = QObject::tr("Unknown reason");
+const QString Strings::CONNECTION_TO_SERVER = QObject::tr("Connection to server...");
+const QString Strings::UNKNOWN_ERROR = QObject::tr("Unknown error");
+const QString Strings::NO_SERVER_RESPONSE = QObject::tr("No server response");
+const QString Strings::ALL_FIELDS_MUST_BE_FILLED_IN = QObject::tr("All fields must be filled in");
+const QString Strings::SERVER_USE_THE_SETTINGS = QObject::tr("The server will use the default settings");
+const QString Strings::MESSAGE_NOT_SELECTED = QObject::tr("Message not selected");
+const QString Strings::FAILED_TO_DISCONNECT_THE_USER = QObject::tr("Failed to disconnect the user");
+const QString Strings::USER_SESSION_IS_DISABLED = QObject::tr("User session is disabled");
+const QString Strings::BAN = QObject::tr("Ban");
+const QString Strings::UNBAN = QObject::tr("Unban");
+const QString Strings::NOT_APPLICABLE_FOR_SERVICE_USERS = QObject::tr("Not applicable for service users");
+const QString Strings::CANNOT_DELETE_SERVICE_USERS = QObject::tr("Cannot delete service users");
+const QString Strings::USER_NOT_SELECTED = QObject::tr("User not selected");
+const QString Strings::DATABASE_QUERY_ERROR = QObject::tr("Database query error");
+const QString Strings::DATABASE_ERROR = QObject::tr("Database error");
+const QString Strings::FAILED_CONNECTION_TO_THE_DATABASE = QObject::tr("Failed connection to the database");
+const QString Strings::CONNECTION_TO_THE_DATABASE_WAS_SUCCESSFUL = QObject::tr("Connection to the database was successful");
+const QString Strings::CONNECTING_TO_THE_DATABASE_SERVER = QObject::tr(
+    "Connecting to the database server...");
+const QString Strings::SETTINGS_SAVED_RESTART_THE_SERVER = QObject::tr(
+    "Settings are saved, restart the server.");
+const QString Strings::SERVER_INI_NOTFOUND = QObject::tr("File server.ini not found");
+const QString Strings::DB_USE_DEFAULT_SETTINGS = QObject::tr(
+    "Default settings will be used to connect to the database");
+const QString Strings::SETTINGS_CANNOT_BE_SAVED = QObject::tr("Settings cannot be saved");
+const QString Strings::FAILED_TO_SAVE_SETTINGS_TO_INI_FILE = QObject::tr(
+    "Failed to save settings to ini file");
+/********/
+
+QString Strings::t(const QString &s)
+{
+    return QCoreApplication::translate("QObject", s.toUtf8().constData());
+}
---
+++ Client/Strings/strings.h
@@ -0,0 +1,47 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+#include <QApplication>
+#include <QObject>
+#include <QString>
+#include <QTranslator>
+
+/*!
+ * \brief Класс для хранения строковых переменных. Для них работает перевод.
+ */
+class Strings : public QObject
+{
+private:
+public:
+    /*Strings*/
+	static const QString EMAIL_BUSY;
+	static const QString LOGIN_BUSY;
+	static const QString UNKNOWN_REASON;
+    static const QString CONNECTION_TO_SERVER;
+    static const QString UNKNOWN_ERROR;
+    static const QString NO_SERVER_RESPONSE;
+	static const QString ALL_FIELDS_MUST_BE_FILLED_IN;
+	static const QString SERVER_USE_THE_SETTINGS;
+	static const QString MESSAGE_NOT_SELECTED;
+	static const QString FAILED_TO_DISCONNECT_THE_USER;
+	static const QString USER_SESSION_IS_DISABLED;
+	static const QString BAN;
+	static const QString UNBAN;
+	static const QString NOT_APPLICABLE_FOR_SERVICE_USERS;
+	static const QString CANNOT_DELETE_SERVICE_USERS;
+	static const QString USER_NOT_SELECTED;
+	static const QString DATABASE_QUERY_ERROR;
+	static const QString DATABASE_ERROR;
+	static const QString FAILED_CONNECTION_TO_THE_DATABASE;
+	static const QString CONNECTION_TO_THE_DATABASE_WAS_SUCCESSFUL;	
+    static const QString THE_TEST_TEXT;
+    static const QString CONNECTING_TO_THE_DATABASE_SERVER;
+    static const QString SETTINGS_SAVED_RESTART_THE_SERVER;
+    static const QString SERVER_INI_NOTFOUND;
+    static const QString DB_USE_DEFAULT_SETTINGS;
+    static const QString SETTINGS_CANNOT_BE_SAVED;
+    static const QString FAILED_TO_SAVE_SETTINGS_TO_INI_FILE;
+    /********/
+    static QString t(const QString &s);
+};
+
+#endif // STRINGS_H
