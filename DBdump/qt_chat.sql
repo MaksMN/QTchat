791bd9decd9ca987636d6d235ad129dcd16b3c5c
MaksMN <maksimovmn78@gmail.com>
2023-10-25 20:40:36 +0300

    db dump

--- DBdump/qt_chat.sql
+++ DBdump/qt_chat.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2023-10-17 18:41:34
+-- Started on 2023-10-25 20:39:16
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -46,11 +46,11 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.private_messages (
     id bigint NOT NULL,
-    author_id bigint,
-    recipient_id bigint,
+    author_id bigint DEFAULT 0,
+    recipient_id bigint DEFAULT 0,
     text text,
-    published bigint,
-    status integer
+    published bigint DEFAULT 0,
+    status integer DEFAULT 4
 );
 
 
@@ -78,10 +78,10 @@ ALTER TABLE public.private_messages ALTER COLUMN id ADD GENERATED ALWAYS AS IDEN
 
 CREATE TABLE public.pub_messages (
     id bigint NOT NULL,
-    author_id bigint,
+    author_id bigint DEFAULT 0,
     text text NOT NULL,
-    published bigint,
-    status integer NOT NULL
+    published bigint DEFAULT 0,
+    status integer DEFAULT 2
 );
 
 
@@ -114,7 +114,7 @@ CREATE TABLE public.users (
     first_name character varying(100) NOT NULL,
     last_name character varying(100),
     registered bigint DEFAULT 0,
-    status integer DEFAULT 2,
+    status integer DEFAULT 1,
     session_key bigint DEFAULT 0,
     hash character varying(40) DEFAULT 0 NOT NULL,
     salt character varying(40) DEFAULT 0 NOT NULL
@@ -139,65 +139,77 @@ ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 4862 (class 0 OID 16550)
+-- TOC entry 4869 (class 0 OID 16550)
 -- Dependencies: 215
 -- Data for Name: private_messages; Type: TABLE DATA; Schema: public; Owner: qt_chat
 --
 
 COPY public.private_messages (id, author_id, recipient_id, text, published, status) FROM stdin;
+0	4	3	привет	1698252862	4
+1	4	3	Как дела?	1698252872	4
+2	3	4	Да все отлично. Чат убойный	1698252923	4
 \.
 
 
 --
--- TOC entry 4863 (class 0 OID 16555)
+-- TOC entry 4870 (class 0 OID 16555)
 -- Dependencies: 216
 -- Data for Name: pub_messages; Type: TABLE DATA; Schema: public; Owner: qt_chat
 --
 
 COPY public.pub_messages (id, author_id, text, published, status) FROM stdin;
+0	1	Всем привет, рад сообщить об открытии нашего нового чата	1698252428	2
+1	3	Ну всё. Звоните Дурову	1698252511	2
+3	3	Скажите ему пусть он свой телеграм закрывает. Теперь у нас есть этот чат	1698252576	2
+4	2	Тогда и Цукерберг пусть свой ватсап закрывает	1698252666	2
+2	2	А зачем?	1698252535	2
 \.
 
 
 --
--- TOC entry 4864 (class 0 OID 16560)
+-- TOC entry 4871 (class 0 OID 16560)
 -- Dependencies: 217
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: qt_chat
 --
 
 COPY public.users (id, login, email, first_name, last_name, registered, status, session_key, hash, salt) FROM stdin;
-1	admin	admin@admin	Администратор	Администраторов	0	1	0	2cfee2bd8b52a3800e594c6aae4deff49b6ac690	019UL67jUe3u9js7LKDz00QKUXuwMSM24m5Y062I
+0	deleted	del	DELETED	\N	0	1	0	0	0
+2	vasya	vasya@mail.ru	Василий	Николаевич	1698252363	1	6027818246678170790	ca2e4601dd77edeb81771b912cf60cd0a9112724	1f4173a2bf6011735757eaaad1b95841c1ca8a33
+1	admin	admin	Администратор	Администраторов	1	4	5015222759454844145	8cda2ca21cd4c6919ffd143624bdde7cd2093f3f	ASDFG
+3	igor	емайл	Игорь	Владимирович	1698252479	1	6028385293768946497	4b92dc693eccdc5d274fb81db7abf57e086a029d	64b9ffc7d8591f29773181de2e6bb9b765263f07
+4	max	емайлd	Максим	Николаевич	1698252834	1	6358358490296423829	e93addd2bac6b64009f3a4fe6f95f5ae550575da	a098b9368e053febadc3b2bb5843133ac00d6997
 \.
 
 
 --
--- TOC entry 4873 (class 0 OID 0)
+-- TOC entry 4880 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: private_messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: qt_chat
 --
 
-SELECT pg_catalog.setval('public.private_messages_id_seq', 0, false);
+SELECT pg_catalog.setval('public.private_messages_id_seq', 2, true);
 
 
 --
--- TOC entry 4874 (class 0 OID 0)
+-- TOC entry 4881 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: pub_messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: qt_chat
 --
 
-SELECT pg_catalog.setval('public.pub_messages_id_seq', 0, false);
+SELECT pg_catalog.setval('public.pub_messages_id_seq', 4, true);
 
 
 --
--- TOC entry 4875 (class 0 OID 0)
+-- TOC entry 4882 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: qt_chat
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 0, false);
+SELECT pg_catalog.setval('public.users_id_seq', 4, true);
 
 
 --
--- TOC entry 4706 (class 2606 OID 16570)
+-- TOC entry 4713 (class 2606 OID 16570)
 -- Name: private_messages private_messages_pk; Type: CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
@@ -206,7 +218,7 @@ ALTER TABLE ONLY public.private_messages
 
 
 --
--- TOC entry 4709 (class 2606 OID 16572)
+-- TOC entry 4716 (class 2606 OID 16572)
 -- Name: pub_messages pub_messages_pk; Type: CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
@@ -215,7 +227,7 @@ ALTER TABLE ONLY public.pub_messages
 
 
 --
--- TOC entry 4714 (class 2606 OID 16574)
+-- TOC entry 4721 (class 2606 OID 16574)
 -- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
@@ -224,7 +236,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4704 (class 1259 OID 16575)
+-- TOC entry 4711 (class 1259 OID 16575)
 -- Name: private_messages_id_idx; Type: INDEX; Schema: public; Owner: qt_chat
 --
 
@@ -232,7 +244,7 @@ CREATE UNIQUE INDEX private_messages_id_idx ON public.private_messages USING btr
 
 
 --
--- TOC entry 4707 (class 1259 OID 16576)
+-- TOC entry 4714 (class 1259 OID 16576)
 -- Name: pub_messages_id_idx; Type: INDEX; Schema: public; Owner: qt_chat
 --
 
@@ -240,7 +252,7 @@ CREATE UNIQUE INDEX pub_messages_id_idx ON public.pub_messages USING btree (id);
 
 
 --
--- TOC entry 4710 (class 1259 OID 16577)
+-- TOC entry 4717 (class 1259 OID 16577)
 -- Name: users_email_idx; Type: INDEX; Schema: public; Owner: qt_chat
 --
 
@@ -248,7 +260,7 @@ CREATE UNIQUE INDEX users_email_idx ON public.users USING btree (email);
 
 
 --
--- TOC entry 4711 (class 1259 OID 16578)
+-- TOC entry 4718 (class 1259 OID 16578)
 -- Name: users_id_idx; Type: INDEX; Schema: public; Owner: qt_chat
 --
 
@@ -256,7 +268,7 @@ CREATE UNIQUE INDEX users_id_idx ON public.users USING btree (id);
 
 
 --
--- TOC entry 4712 (class 1259 OID 16579)
+-- TOC entry 4719 (class 1259 OID 16579)
 -- Name: users_login_idx; Type: INDEX; Schema: public; Owner: qt_chat
 --
 
@@ -264,7 +276,7 @@ CREATE UNIQUE INDEX users_login_idx ON public.users USING btree (login);
 
 
 --
--- TOC entry 4718 (class 2620 OID 16580)
+-- TOC entry 4725 (class 2620 OID 16580)
 -- Name: users after_delete; Type: TRIGGER; Schema: public; Owner: qt_chat
 --
 
@@ -272,7 +284,7 @@ CREATE TRIGGER after_delete BEFORE DELETE ON public.users FOR EACH ROW EXECUTE F
 
 
 --
--- TOC entry 4715 (class 2606 OID 16581)
+-- TOC entry 4722 (class 2606 OID 16581)
 -- Name: private_messages private_messages_fk; Type: FK CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
@@ -281,7 +293,7 @@ ALTER TABLE ONLY public.private_messages
 
 
 --
--- TOC entry 4716 (class 2606 OID 16586)
+-- TOC entry 4723 (class 2606 OID 16586)
 -- Name: private_messages private_messages_fk2; Type: FK CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
@@ -290,15 +302,15 @@ ALTER TABLE ONLY public.private_messages
 
 
 --
--- TOC entry 4717 (class 2606 OID 16591)
+-- TOC entry 4724 (class 2606 OID 16607)
 -- Name: pub_messages pub_messages_fk; Type: FK CONSTRAINT; Schema: public; Owner: qt_chat
 --
 
 ALTER TABLE ONLY public.pub_messages
-    ADD CONSTRAINT pub_messages_fk FOREIGN KEY (author_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE SET NULL;
+    ADD CONSTRAINT pub_messages_fk FOREIGN KEY (author_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE SET DEFAULT;
 
 
--- Completed on 2023-10-17 18:41:34
+-- Completed on 2023-10-25 20:39:16
 
 --
 -- PostgreSQL database dump complete
